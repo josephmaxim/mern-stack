1bba4d3a5a66b33fe5f41b3361ae75d09a22e44f
Joseph Maxim <josephmdagunan@gmail.com>
2018-02-17 23:58:37 -0500

    converter Profile page to a stateless component

--- src/components/user/ProfilePage.jsx
+++ src/components/user/ProfilePage.jsx
@@ -1,17 +1,5 @@
 import React from 'react';
 
-export default class ProfilePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // init state here
-    };
-  }
-  render() {
-    return (
-      <div>
-        <h1>Profile Page</h1>
-      </div>
-    );
-  }
-}
+const renderApp = () => <div><h1>Profile Page</h1></div>;
+
+export default renderApp;
